25510e9b7b7867526d68880b7ec254726f34a159
Sameer <sameer@Sameers-MacBook-Pro.local>
2020-04-21 16:21:23 +0530

    integration with cognito api Add role n admin

--- frontend/src/app/sAdmin/add-admin/add-admin.component.ts
+++ frontend/src/app/sAdmin/add-admin/add-admin.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import * as AWS from 'aws-sdk';
+AWS.config.update({region:'us-east-2',secretAccessKey: 'Bk3UhOP0Okei2Y9kbwQgobpCdlB4hLRtpfjACU+6',
+accessKeyId: 'AKIA4SAVCJANYHGMDTPZ'});
+
 
 @Component({
   selector: 'app-add-admin',
@@ -9,19 +12,16 @@ import * as AWS from 'aws-sdk';
 })
 export class AddAdminComponent implements OnInit {
 
-  subscribers:any = [];
+  subscribers: any = [];
   private baseURL = "/api";
   constructor(private http: HttpClient) { }
 
-  AWS.config.apiVersions = {
-  cognitoidentityserviceprovider: '2016-04-18',
-  // other service API versions
-};
 
-var cognitoserviceidentityprovider = new AWS.CognitoIdentityServiceProvider();
 
-  ngOnInit(){
-   var token = window.localStorage.getItem('tokenID')
+  cognitoserviceidentityprovider = new AWS.CognitoIdentityServiceProvider( { apiVersion: '2016-04-18'});
+
+  ngOnInit() {
+    var token = window.localStorage.getItem('tokenID')
     var header = {
       headers: new HttpHeaders().set('Authorization', `Bearer ${token}`)
     }
@@ -31,38 +31,38 @@ var cognitoserviceidentityprovider = new AWS.CognitoIdentityServiceProvider();
   }
 
 
-  onSubmit(adminForm){
-  var params = {
-  UserPoolId: 'us-east-2_hKGBBXcu3'
-  Username: adminForm.name,
-  DesiredDeliveryMediums: [
-       "EMAIL"
-  ],
-  ForceAliasCreation: false,
-  MessageAction: "RESEND",
-  TemporaryPassword: adminForm.tempPass,
-  UserAttributes: [
-    {
-      Name: 'email', /* required */
-      Value: adminForm.email
-    },
-    {
-    Name: 'custom:role',
-    Value: 'admin'
-    }
-   ]
-  };
+  onSubmit(adminForm) {
+    var params = {
+      UserPoolId: 'us-east-2_hKGBBXcu3',
+      Username: adminForm.value.name,
+      DesiredDeliveryMediums: [
+        "EMAIL"
+      ],
+      ForceAliasCreation: false,
+      // MessageAction: "RESEND",
+      TemporaryPassword: adminForm.value.tempPass,
+      UserAttributes: [
+        {
+          Name: 'email', /* required */
+          Value: adminForm.value.email
+        },
+        {
+          Name: 'custom:role',
+          Value: 'admin'
+        }
+      ]
+    };
 
-  cognitoidentityserviceprovider.adminCreateUser(params, function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     console.log(data);           // successful response
-  });
+    this.cognitoserviceidentityprovider.adminCreateUser(params, function (err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else console.log(data);           // successful response
+    });
 
     console.log(adminForm.value)
     var token = window.localStorage.getItem('tokenID')
     var header = {
       headers: new HttpHeaders().set('Authorization', `Bearer ${token}`)
     }
-    this.http.post(this.baseURL + '/superAdmin/addAdmin',adminForm.value, header).subscribe()
+    this.http.post(this.baseURL + '/superAdmin/addAdmin', adminForm.value, header).subscribe()
   }
 }
