d057d470636fbc20fce651f367dfdac886ad8556
Sameer <sameer@Sameers-MacBook-Pro.local>
2020-04-21 14:40:51 +0530

    addrole, addAdmin - tempPass field

--- routes/superAdmin.js
+++ routes/superAdmin.js
@@ -8,7 +8,7 @@ const CognitoExpress = require("cognito-express");
 
 const cognitoExpress = new CognitoExpress({
     region: "us-east-2",
-    cognitoUserPoolId: "us-east-2_hKGBBXcu3",
+    cognitoUserPoolId: "us-east-2_SDqqsNinX",
     tokenUse: "id", //Possible Values: access | id
     tokenExpiration: 3600000 //Up to default expiration of 1 hour (3600000 ms)
 });
